6466c2146278a958e6595b8ad159461d2736ab01
Gustavo Maia Moreira <109545152+BigPapa6911@users.noreply.github.com>
2024-08-07 15:21:35 -0300

    Finalização de desafio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone5 = new Iphone("9182638", "iphone 5", "234124", 16);
+Nokia nokia1 = new Nokia("1278123", "nokia 1 ", "12879634", 8);
+
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Whatsapp");
